58f381f8ce1fa87483ae61258bc0c2294ce4c46d
Martin Romo <romoma@usc.edu>
2020-03-19 19:11:52 -0500

    Renamed file to lab4.c

--- lab4.c
+++ lab4.c
@@ -1,23 +1,3 @@
-/*************************************************************
-*       at328-0.c - Demonstrate simple I/O functions of ATmega328
-*
-*       Program loops turning PC0 on and off as fast as possible.
-*
-* The program should generate code in the loop consisting of
-*   LOOP:   SBI  PORTC,0        (2 cycles)
-*           CBI  PORTC,0        (2 cycles)
-*           RJMP LOOP           (2 cycles)
-*
-* PC0 will be low for 4 / XTAL freq
-* PC0 will be high for 2 / XTAL freq
-* A 9.8304MHz clock gives a loop period of about 600 nanoseconds.
-*
-* Revision History
-* Date     Author      Description
-* 09/14/12 A. Weber    Initial Release
-* 11/18/13 A. Weber    Renamed for ATmega328P
-*************************************************************/
-
 #include <avr/io.h>
 
 #define FREQ 7372800
